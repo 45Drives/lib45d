7249f7941f2a76aaae5904cd5139c28c4eb7cc77
joshuaboud <joshuaboud@hotmail.com>
2021-07-27 11:01:55 -0300

    add license headers

--- src/impl/config_parser.cpp
+++ src/impl/config_parser.cpp
@@ -1,3 +1,22 @@
+/*
+	Copyright (C) 2021 Joshua Boudreau <jboudreau@45drives.com>
+
+	This file is part of lib45d_conf.
+
+	lib45d_conf is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	lib45d_conf is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with lib45d_conf.  If not, see <https://www.gnu.org/licenses/>.
+ */
+ 
 #include "45d_conf.hpp"
 #include "parser_functions.hpp"
 
--- src/incl/45d_conf.hpp
+++ src/incl/45d_conf.hpp
@@ -1,20 +1,20 @@
 /*
  *    Copyright (C) 2021 Joshua Boudreau <jboudreau@45drives.com>
  *    
- *    This file is part of 45d_conf.
+ *    This file is part of lib45d_conf.
  * 
- *    45d_conf is free software: you can redistribute it and/or modify
+ *    lib45d_conf is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
  *    the Free Software Foundation, either version 3 of the License, or
  *    (at your option) any later version.
  * 
- *    45d_conf is distributed in the hope that it will be useful,
+ *    lib45d_conf is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *    GNU General Public License for more details.
  * 
  *    You should have received a copy of the GNU General Public License
- *    along with 45d_conf.  If not, see <https://www.gnu.org/licenses/>.
+ *    along with lib45d_conf.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #pragma once
